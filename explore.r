dfb60ee60412541f0bc6bf31c8f6e5d0fda6a542
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-05-09 09:00:31 +0100

    Update explore.r
    change order, historgram, barcharts, facet_wrap

--- explore.r
+++ explore.r
@@ -1,1 +1,21 @@
+# Load package
+library(ggplot2)
 
+# CATEGORIAL DATA
+
+# Change the order of the levels in align
+data$variable1 <- factor(data$variable1, 
+                       levels = c('Bad', 'Neutral', 'Good'))
+
+# Conditional bar chart is the break down of one variable with another variable
+# Plot of variable 1 broken down by variable 2
+ggplot(data = data, aes(x = variable1)) + 
+  geom_bar() +
+  facet_wrap(~ variable2)
+
+# NUMERICAL DATA
+
+# Create faceted histogram
+ggplot(data, aes(x = variable1)) +
+  geom_histogram() +
+  facet_wrap(~ variable2)
